9fdaecc35a52d55e5f38d91921ed7be29edebffa
Cameron Teasdale <cameron.teasdale@shopify.com>
2020-02-27 18:45:36 -0500

    fix iconography

--- gatsby-config.js
+++ gatsby-config.js
@@ -188,7 +188,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/cam.jpg'
+        icon: 'static/ale.png'
       },
     },
     'gatsby-plugin-offline',
